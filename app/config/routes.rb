52537ea4206e88c69beee11a1c0db77c6af5b292
werrnnnwerrrnnnnnn <swbt.tatiya@gmail.com>
2025-04-19 01:52:50 +0700

    feat: generate ChatRoom and Message scaffolds with ChatRoomChannel

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :messages
+  resources :chat_rooms
   resources :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
